3eab125b149a3986487996c8fc37947046fb1777
Lukas <lukas.huber@stud.hslu.ch>
2023-02-16 19:54:24 +0100

    changed styling

--- pages/administration.tsx
+++ pages/administration.tsx
@@ -25,14 +25,11 @@ export default function Administration({technologies}: { technologies: Technolog
   return (
     <Layout home={false}>
       <h5>Administration</h5>
-      <nav>
-        <ul>
-        </ul>
-        <ul>
-          <button>ADD</button>
-        </ul>
-      
-      </nav>
+      <div style={{display: "flex", alignItems: "end"}}>
+        <div style={{marginLeft: "auto"}}>
+          <button className="outline" style={{marginBottom: "0px"}}>ADD</button>
+        </div>
+      </div>
       <table role="grid">
         <thead>
           <tr>
@@ -50,7 +47,7 @@ export default function Administration({technologies}: { technologies: Technolog
               <td>{technology.name}</td>
               <td>{Category[technology.category]}</td>
               <td>{Ring[technology.ring]}</td>
-              <td style={{textAlign: "right"}}><a href="#" role="button" className="outline">EDIT</a> <a href="#" role="button" className="outline contrast" onClick={() => deleteTechnology(technology.id ?? "")}>DELETE</a></td>
+              <td style={{textAlign: "right"}}><a href="#" role="button" className="outline">EDIT</a> <a href="#" role="button" className="outline" style={{color: "#c62828", borderColor: "#c62828", marginLeft: "5px"}} onClick={() => deleteTechnology(technology.id ?? "")}>DELETE</a></td>
             </tr>
           ))}
         </tbody>
